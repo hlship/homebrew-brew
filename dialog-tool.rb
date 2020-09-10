26bbbfaf077adc95e02c63dd0f12ddeabc994afe
Howard Lewis Ship <h@lewisship.net>
2020-09-09 22:17:27 -0700

    dialog-tool v1.2

--- dialog-tool.rb
+++ dialog-tool.rb
@@ -1,8 +1,8 @@
 class DialogTool < Formula
   desc "Tool to assist in testing and building games using Dialog"
   homepage "https://github.com/hlship/dialog-tool"
-  url "https://github.com/hlship/dialog-tool/archive/v1.1.zip"
-  sha256 "3840b9589a698ecd57821b7b7298e90dd0e50282558b74691fa3dde75189d645"
+  url "https://github.com/hlship/dialog-tool/archive/v1.2.zip"
+  sha256 "910e7d4bdd8c4b2ed5aab3727278d4afffcac429db18c2e2d1d5843d20958b8e"
 
   depends_on "candid82/brew/joker"
   depends_on "colordiff"
