1486562a0c76b88058999327aca39ae4ea96e80c
Howard Lewis Ship <hlship@gmail.com>
2024-10-13 16:50:13 -0700

    dialog-tool 2.0-alpha-4

--- dialog-tool.rb
+++ dialog-tool.rb
@@ -1,10 +1,10 @@
 class DialogTool < Formula
   desc "Tool to assist in testing and building games using Dialog"
   homepage "https://github.com/hlship/dialog-tool"
-  url "https://github.com/hlship/dialog-tool/releases/download/2.0-alpha-3/dialog-tool-2.0-alpha-3.zip"
-  sha256 "969821b815cb31a8a072275b52fd6dcf5f11dfbbc921a91c01a41f77c0cb3a22"
+  url "https://github.com/hlship/dialog-tool/releases/download/2.0-alpha-4/dialog-tool-2.0-alpha-4.zip"
+  sha256 "4cd6ccba5bcb248f3190cd80fd093ac3a7ed7bab72deb2754d8570df6b5e1cb3"
 
-  version "2.0-alpha-3"
+  version "2.0-alpha-4"
 
   depends_on "vickio/dialog/dialog-if"
   depends_on "hlship/brew/aamachine"
