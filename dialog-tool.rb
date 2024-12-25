b36e76298d8888dd714c622cccb6077cb2edd341
Howard Lewis Ship <hlship@gmail.com>
2024-12-24 18:32:53 -0800

    dialog-tool 2.0-alpha-6

--- dialog-tool.rb
+++ dialog-tool.rb
@@ -1,10 +1,10 @@
 class DialogTool < Formula
   desc "Tool to assist in testing and building games using Dialog"
   homepage "https://github.com/hlship/dialog-tool"
-  url "https://github.com/hlship/dialog-tool/releases/download/2.0-alpha-5/dialog-tool-2.0-alpha-5.zip"
-  sha256 "2a8ab8698d25e081d2574d51b5d037e49c3c7c41ed1803f005a2eca5a68338f4"
+  url "https://github.com/hlship/dialog-tool/releases/download/2.0-alpha-6/dialog-tool-2.0-alpha-6.zip"
+  sha256 "9198d3a217b08aa7b65beecd378fd2f94442e92c2f99dd5e34e19f8054fc115f"
 
-  version "2.0-alpha-5"
+  version "2.0-alpha-6"
 
   depends_on "vickio/dialog/dialog-if"
   depends_on "hlship/brew/aamachine"
@@ -18,6 +18,12 @@ class DialogTool < Formula
       bin.install "resources"
       bin.install "skein-ui"
       bin.install "bb.edn"
+  end
+
+  def post_install
+    # This will slow down the install by downloading dgt's dependencies, but means first time 
+    # startup is faster.
+      system "#{bin}/dgt -h > /dev/null 2>&1"
   end
 
   test do
