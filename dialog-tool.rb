e49065fe996b0c3fb9821e8175eb3a8f08d1dc62
Howard Lewis Ship <hlship@gmail.com>
2025-01-05 14:33:37 -0800

    dialog-tool 2.0-alpha-7

--- dialog-tool.rb
+++ dialog-tool.rb
@@ -1,15 +1,16 @@
 class DialogTool < Formula
   desc "Tool to assist in testing and building games using Dialog"
   homepage "https://github.com/hlship/dialog-tool"
-  url "https://github.com/hlship/dialog-tool/releases/download/2.0-alpha-6/dialog-tool-2.0-alpha-6.zip"
-  sha256 "9198d3a217b08aa7b65beecd378fd2f94442e92c2f99dd5e34e19f8054fc115f"
+  url "https://github.com/hlship/dialog-tool/releases/download/2.0-alpha-7/dialog-tool-2.0-alpha-7.zip"
+  sha256 "82b7737305fc91ca963afd9399275f41128a11314d9cade3336ebec58f0f1cd6"
 
-  version "2.0-alpha-6"
+  version "2.0-alpha-7"
 
   depends_on "vickio/dialog/dialog-if"
   depends_on "hlship/brew/aamachine"
   depends_on "borkdude/brew/babashka"
   depends_on "imagemagick"
+  depends_on "frotz"
 
 
   def install
@@ -21,9 +22,9 @@ class DialogTool < Formula
   end
 
   def post_install
-    # This will slow down the install by downloading dgt's dependencies, but means first time 
-    # startup is faster.
-      system "#{bin}/dgt -h > /dev/null 2>&1"
+      # This will slow down the install by downloading dgt's dependencies, but means first time 
+      # startup is faster.
+      # system "#{bin}/dgt -h > /dev/null 2>&1"
   end
 
   test do
