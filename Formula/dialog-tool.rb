ea83b9be04b2c6c30fd9844fadb9d73c6896b9a5
Howard M. Lewis Ship <hlship@gmail.com>
2026-02-16 06:21:08 -0800

    dialog-tool 2.0-alpha-11

--- Formula/dialog-tool.rb
+++ Formula/dialog-tool.rb
@@ -1,20 +1,20 @@
 class DialogTool < Formula
   desc "Assist building projects for the Dialog programming language"
   homepage "https://github.com/hlship/dialog-tool"
-  url "https://github.com/hlship/dialog-tool/releases/download/2.0-alpha-10/dialog-tool-2.0-alpha-10.zip"
-  sha256 "d91ada75992c8134e5e47c00e8dfc39d0e20e8e220459a62c1fde77929d531fc"
+  url "https://github.com/hlship/dialog-tool/releases/download/2.0-alpha-11/dialog-tool-2.0-alpha-11.zip"
+  sha256 "f2c434a5371cf63e0db10e22af19f9d0d70e883426016e40f0f8d30b1bd0f567"
 
   depends_on "borkdude/brew/babashka"
   depends_on "dialog-if/brew/aamachine"
   depends_on "dialog-if/brew/dialog-if"
   depends_on "frotz"
   depends_on "imagemagick"
+  depends_on "java"
 
   def install
-      bin.install "dgt"
-      bin.install "src"
-      bin.install "resources"
       bin.install "bb.edn"
+      bin.install "dgt"
+      bin.install "dialog-tool-2.0-alpha-11.zip"
 
       zsh_completion_file = buildpath/"_dgt"
 
