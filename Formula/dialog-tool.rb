79fc619a8cc95eda1047a61542cd1a990726883f
Howard M. Lewis Ship <hlship@gmail.com>
2026-02-16 10:37:19 -0800

    Adjust order of installation

--- Formula/dialog-tool.rb
+++ Formula/dialog-tool.rb
@@ -15,8 +15,8 @@ class DialogTool < Formula
 
   def install
       bin.install "bb.edn"
-      bin.install "dgt"
       bin.install "dialog-tool-2.0-alpha-11.zip"
+      bin.install "dgt"
 
       zsh_completion_file = buildpath/"_dgt"
 
