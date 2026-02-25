124deba8eea2b1e0e1cebeab655ae78f7c1226da
Howard M. Lewis Ship <hlship@gmail.com>
2026-02-25 10:39:39 -0800

    dialog-tool 2.0-beta-1

--- Formula/dialog-tool.rb
+++ Formula/dialog-tool.rb
@@ -1,9 +1,9 @@
 class DialogTool < Formula
   desc "Assist building projects for the Dialog programming language"
   homepage "https://github.com/hlship/dialog-tool"
-  url "https://github.com/hlship/dialog-tool/releases/download/2.0-alpha-11/dialog-tool-2.0-alpha-11.zip"
-  version "2.0-alpha-11"
-  sha256 "dcbc30b75961fee80e197b53df4eafbe17f9dba060221432149774c54b125424"
+  url "https://github.com/hlship/dialog-tool/releases/download/2.0-beta-1/dialog-tool-2.0-beta-1.zip"
+  version "2.0-beta-1"
+  sha256 "b5ac3599b01c5be4e3a4607358493a01ba70402e735621ee5352238f80c78ada"
   version_scheme 1 # Because version was borked in some prior releases due to -alpha-
 
   depends_on "borkdude/brew/babashka"
@@ -14,15 +14,14 @@ class DialogTool < Formula
   depends_on "java"
 
   def install
-      bin.install "bb.edn"
-      bin.install "dialog-tool-2.0-alpha-11.jar"
+      bin.install "dialog-tool-2.0-beta-1.jar"
       bin.install "dgt"
 
       zsh_completion_file = buildpath/"_dgt"
 
       ENV["CLI_TOOLS_CACHE_DIR"] = buildpath
 
-      system "bb", bin/"dgt", "completions", zsh_completion_file
+      system bin/"dgt", "completions", zsh_completion_file
 
       zsh_completion.install zsh_completion_file
   end
