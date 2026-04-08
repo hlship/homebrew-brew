a7a58e0db0e96624e1f2be0fbe870a1c9b6022b8
Howard M. Lewis Ship <hlship@gmail.com>
2026-04-07 17:54:18 -0700

    dexter 0.1-alpha-5

--- Formula/dexter.rb
+++ Formula/dexter.rb
@@ -1,14 +1,14 @@
 class Dexter < Formula
   desc "Interactive dependency explorer for Clojure and JVM projects"
   homepage "https://github.com/hlship/dexter"
-  url "https://github.com/hlship/dexter/releases/download/0.1-alpha-3/dexter-0.1-alpha-3.zip"
-  version "0.1-alpha-3"
-  sha256 "a612d42e107994bdb1c6acb6629bf51dc31b4a135e3aa7cffbe0fbc2a168e5ba"
+  url "https://github.com/hlship/dexter/releases/download/0.1-alpha-5/dexter-0.1-alpha-5.zip"
+  version "0.1-alpha-5"
+  sha256 "d97ee7f28f93723b882f46489cf1fc045f8c520254ce1344073ba160f521e48c"
 
   depends_on "java"
 
   def install
-      bin.install "dexter-0.1-alpha-3.jar"
+      bin.install "dexter-0.1-alpha-5.jar"
       bin.install "dexter"
       chmod 0755, bin/"dexter"
   end
