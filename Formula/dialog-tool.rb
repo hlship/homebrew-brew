c9fb623cc7c820c772439251e23a831ffba1dea1
Howard M. Lewis Ship <hlship@gmail.com>
2026-04-19 14:20:10 -0700

    dialog-tool 2.0-beta-10

--- Formula/dialog-tool.rb
+++ Formula/dialog-tool.rb
@@ -1,9 +1,9 @@
 class DialogTool < Formula
   desc "Assist building projects for the Dialog programming language"
   homepage "https://github.com/hlship/dialog-tool"
-  url "https://github.com/hlship/dialog-tool/releases/download/2.0-rc-1/dialog-tool-2.0-rc-1.zip"
-  version "2.0-rc-1"
-  sha256 "5a33b4ad4461b45541038bb28065d2ec683985d6694f18f0724ce5a9e34e84c5"
+  url "https://github.com/hlship/dialog-tool/releases/download/2.0-beta-10/dialog-tool-2.0-beta-10.zip"
+  version "2.0-beta-10"
+  sha256 "dded1c8caa5aafc8b965f4d7edac2ebb501a84026f8797b81de40945707293f9"
   version_scheme 1 # Because version was borked in some prior releases due to -alpha-
 
   depends_on "dialog-if/brew/aamachine"
@@ -13,7 +13,7 @@ class DialogTool < Formula
   depends_on "java"
 
   def install
-      bin.install "dialog-tool-2.0-rc-1.jar"
+      bin.install "dialog-tool-2.0-beta-10.jar"
       bin.install "dgt"
       chmod 0755, bin/"dgt"
 
