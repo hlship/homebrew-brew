993b798a1ee6cbb85cad5c0709f07c89aeaef24c
Howard M. Lewis Ship <hlship@gmail.com>
2026-05-19 14:51:02 -0300

    chore: update dexter to 0.1-beta-2

--- Formula/dexter.rb
+++ Formula/dexter.rb
@@ -1,14 +1,14 @@
 class Dexter < Formula
   desc "Interactive dependency explorer for Clojure and JVM projects"
   homepage "https://github.com/hlship/dexter"
-  url "https://github.com/hlship/dexter/releases/download/0.1-beta-1/dexter-0.1-beta-1.zip"
-  version "0.1-beta-1"
-  sha256 "87182a724025b894e6d7a5a065d9d14436c5be362f1cea13e5050fa76baf3144"
+  url "https://github.com/hlship/dexter/releases/download/0.1-beta-2/dexter-0.1-beta-2.zip"
+  version "0.1-beta-2"
+  sha256 "a81e7e5fe7e44290616ede4cee06113eaf65c3113874f9adae2868fd419730ad"
 
   depends_on "java"
 
   def install
-      bin.install "dexter-0.1-beta-1.jar"
+      bin.install "dexter-0.1-beta-2.jar"
       bin.install "dexter"
       chmod 0755, bin/"dexter"
   end
